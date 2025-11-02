6901bf51d10b5c9d1b5a97cff3e9e589620e76b1
cmllr <mail@chmr.eu>
2025-11-02 12:40:05 +0100

    Drop text action, overhaul catchfile

--- core/actions/__init__.py
+++ core/actions/__init__.py
@@ -12,7 +12,6 @@ __all__ = [
     'authorize',
     'catchfile',
     'servefile',
-    'text',
     'sleep'
 ]
 
--- core/actions/catchfile.py
+++ core/actions/catchfile.py
@@ -1,51 +1,66 @@
 # Copyright (c) 2018 RoastingMalware
 # See the LICENSE file for more information
 
-"""
-Catches uploaded files from HTTP POST requestObjs
-"""
-
-import hashlib
-import os
-
-from os.path import join, isdir, isfile, isabs
+from os.path import join, isdir, isfile
 from datetime import datetime
 from core import config, logging
+from flask import Flask, Request, abort
+from typing import Dict
 
 
-def run(app, selectedPath: str, route: object,
-        requestObj):
+def run(_: Flask, __: str, ___: Dict, request_obj: Request):
     """
-    Executes the catchfile action
+    Catch uploaded files. Relies on the downloadDir setting.
+
+    For each requewst bringing files following files will be created
+
+    - {hash}_{post_field} - The uploaded file(s)
+    - report_{hash}.txt - The Report for this request, listing all collected files with original filename, stored filename, size, content type.
+
+    parameters:
+        request_obj: The Flask request object
+
+    route object:
+        There are no options.
+    
+    returns:
+        404 abort if: File object is None, Download directory is unconfigured, Download directory does not exist.
+    
     """
-    if requestObj.method == 'POST':
-        for key in requestObj.files:
-            file_object = requestObj.files.get(key)
-            # shasum = hashlib.sha224(copy.copy(file_object).read()).hexdigest()
-            filename = str(datetime.now().timestamp())
-            downloadDir = config.getConfigurationValue("honeypot", "downloadDir")
+    report_content = ""
+    report_filename = None
+    for key, file_object in request_obj.files.items():
+        if not file_object:
+            return abort(404)
+        filename = f"{request_obj.__hash__()}_{key}"
+        
+        download_dir = config.getConfigurationValue("honeypot", "downloadDir")
 
-            if not isabs(downloadDir):
-                downloadDir = join(config.ROOT, downloadDir)
-            if not isdir(downloadDir):
-                raise Exception("Download dir not existing")
+        if not download_dir:
+            return abort(404)
+        
+        absolute_download_dir = join(config.ROOT, str(download_dir))
 
-            file_path = join(downloadDir, filename)
-            file_object.save(file_path)
+        if not isdir(absolute_download_dir):
+            return abort(404)
+        
+        if report_filename is None:
+            # we can assume that there is a proper download dir now
+            report_filename = join(absolute_download_dir, f"report_{request_obj.__hash__()}.txt")
 
-            shasum = hashlib.sha256()
-            with open(file_path, 'rb') as f:
-                while True:
-                    data = f.read(65536)
-                    if not data:
-                        break
-                    shasum.update(data)
-            hashed_filepath = join(downloadDir, shasum.hexdigest())
-            os.rename(file_path, hashed_filepath)
+        file_path = join(absolute_download_dir, filename)
+        file_object.save(file_path)
 
-            logging.log(logging.EVENT_ID_UPLOAD,
-                        datetime.now(),
-                        "Uploaded {0}".format(shasum.hexdigest()),
-                        not isfile(file_path),
-                        requestObj.remote_addr,
-                        shasum=shasum.hexdigest())
+        report_content += f"Filename: {file_object.filename}\n" + f"Stored filename: {filename}\n"+  f"Size: {file_object.content_length}\n" + f"Type: {file_object.content_type}\n" + "\n"
+        
+        logging.log(
+            logging.EVENT_ID_UPLOAD,
+            datetime.now(),
+            f"Uploaded file with report {report_filename}",
+            not isfile(file_path),
+            str(request_obj.remote_addr),
+        )
+    # only write a report if there were files
+    if report_filename is not None:
+        with open(report_filename, "w") as file:
+            file.write(report_content)
